f7e527859f06c604b1eac4959bb14e138dd84e87
MayaYume <160130352+MayaYume@users.noreply.github.com>
2024-03-04 21:53:10 -0300

    Update supermercado.py

--- supermercado.py
+++ supermercado.py
@@ -3,21 +3,23 @@ total_volumes = 0  # Inicializa o total de volumes
 
 # Loop para receber a quantidade e o preço unitário de cada item
 while True:
-    quantidade = int(input("Digite a quantidade comprada (ou 0 para finalizar): "))
+    quantidade = int(input())
     
     # Verifica se a quantidade é 0 para finalizar a compra
     if quantidade == 0:
         break
+    else:
     
-    preco_unitario = float(input("Digite o preço unitário: "))
+       preco_unitario = float(input())
     
     # Calcula o subtotal do item e adiciona ao total da compra
-    subtotal = quantidade * preco_unitario
-    total_compra += subtotal
+       subtotal = quantidade * preco_unitario
+       total_compra += subtotal
     
     # Adiciona a quantidade comprada ao total de volumes
-    total_volumes += quantidade
+       total_volumes += quantidade
 
-# Imprime o total da compra e a quantidade de volumes comprados
-print(f"Total da compra: R${total_compra:.2f}")
-print(f"Quantidade de volumes comprados: {total_volumes}")
+if (total_volumes == 1):
+        print(f"Foi comprado {total_volumes} item, totalizando R${total_compra:.2f}")
+elif(total_volumes > 1): 
+        print(f"Foram comprados {total_volumes} itens, totalizando R${total_compra:.2f}")
